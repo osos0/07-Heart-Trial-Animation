e71028de63e13cfe36475ec200fe0394a2c1db36
unknown <osama_magdy0@yahoo.com>
2022-11-15 22:33:25 +0200

    final uploaded from my home

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const bodyEl = document.querySelector("body")
+const bodyEl = document.querySelector("body");
 
-
-bodyEl.addEventListener("mousemove",(event)=>{
-    const xPos = event.offsetX;
-    const yPos = event.offsetY;
-    const spanEl = document.createElement("span");
-    bodyEl.appendChild(spanEl);
-    });
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "xp";
+  spanEl.style.height = size + "xp";
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
